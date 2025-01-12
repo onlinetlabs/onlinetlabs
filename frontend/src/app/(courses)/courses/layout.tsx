64a3cea34671ad87874cfe6c072db707e6ae9da9
nevsky118 <nevsky118@gmail.com>
2025-01-12 16:28:50 +0300

    add: footer

--- frontend/src/app/(courses)/courses/layout.tsx
+++ frontend/src/app/(courses)/courses/layout.tsx
@@ -1,3 +1,4 @@
+import { SiteFooter } from "@widgets/site-footer"
 import { SiteHeader } from "@widgets/site-header"
 
 interface CoursesLayoutProps {
@@ -13,6 +14,7 @@ export default function CoursesLayout({ children }: CoursesLayoutProps) {
       <div className="flex flex-col flex-1 mx-auto w-full border-border/40 dark:border-border min-[1800px]:max-w-[1536px] min-[1800px]:border-x">
         <SiteHeader sticky={false} />
         <main className="flex-1">{children}</main>
+        <SiteFooter />
       </div>
     </div>
   )
