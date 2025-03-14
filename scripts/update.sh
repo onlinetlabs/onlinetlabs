e26206c3f1f7be2f15bec01989b1525a334c5ed2
ixenion <stridjj@gmail.com>
2025-03-14 16:14:54 +0300

    Managing scripts improoved.

--- scripts/update.sh
+++ scripts/update.sh
@@ -1,28 +1,16 @@
-# Stopping and deleting DIAG containers:
-echo "[*] Stopping and deleting DIAG containers..."
-./scripts/docker_delete.sh
-if [ $? -eq 0 ]; then
-    echo "[✔] Stopping and deleting DIAG containers: Done."
-else
-    echo "[✘] Stopping and deleting DIAG containers: Error. Try manual."
-fi
-
-
-
-# Restoring repository to default:
 echo "[*] Restoring local repo..."
 git restore .
-if [ $? -eq 0 ]; then
-    echo "[✔] Restoring local repo: Done!"
-else
-    echo "[✘] Restoring local repo: Error. Try manual."
+if [ $? -ne 0 ]; then
+    echo "[✘] ERROR: Restoring local repo. Exiting."
+    exit
 fi
 
-# Updating repository:
+
 echo "[*] Updating local repo..."
 git pull
-if [ $? -eq 0 ]; then
-    echo "[✔] Updating local repo: Done!"
-else
-    echo "[✘] Updating local repo: Error. Try manual."
+if [ $? -ne 0 ]; then
+    echo "[✘] ERROR: Updating local repo. Exiting."
+    exit
 fi
+
+./scripts/docker_deploy.sh
