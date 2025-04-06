d00c6b33aabbe054f84a7364287b300ff51326a5
nevsky118 <nevsky118@gmail.com>
2025-04-06 13:54:13 +0300

    fix: style

--- frontend/src/app/forbidden.tsx
+++ frontend/src/app/forbidden.tsx
@@ -13,10 +13,10 @@ export default function Forbidden() {
     <main className="bg-background-100 relative mx-auto flex min-h-svh w-full max-w-5xl flex-col justify-center gap-6 p-6 md:p-10">
     <div className="text-center">
       <p className="text-base font-semibold text-ds-blue-900">403</p>
-      <h1 className="mt-4 text-5xl font-semibold tracking-tight text-balance text-gray-900 sm:text-7xl">
+      <h1 className="mt-4 text-5xl font-semibold tracking-tight text-balance text-foreground sm:text-7xl">
         Нет доступа
       </h1>
-      <p className="mt-6 text-lg font-medium text-pretty text-gray-500 sm:text-xl/8">
+      <p className="mt-6 text-lg font-medium text-pretty text-muted-foreground sm:text-xl/8">
         Вы не авторизованы для доступа к этому ресурсу.
       </p>
       <div className="mt-10 flex items-center justify-center gap-x-6">
--- frontend/src/app/unauthorized.tsx
+++ frontend/src/app/unauthorized.tsx
@@ -13,10 +13,10 @@ export default function Unauthorized() {
     <main className="bg-background-100 relative mx-auto flex min-h-svh w-full max-w-5xl flex-col justify-center gap-6 p-6 md:p-10">
     <div className="text-center">
       <p className="text-base font-semibold text-ds-blue-900">401</p>
-      <h1 className="mt-4 text-5xl font-semibold tracking-tight text-balance text-gray-900 sm:text-7xl">
+      <h1 className="mt-4 text-5xl font-semibold tracking-tight text-balance text-foreground sm:text-7xl">
         Нет доступа
       </h1>
-      <p className="mt-6 text-lg font-medium text-pretty text-gray-500 sm:text-xl/8">
+      <p className="mt-6 text-lg font-medium text-pretty text-muted-foreground sm:text-xl/8">
         Пожалуйста, войдите в аккаунт, чтобы получить доступ к этой странице.
       </p>
       <div className="mt-10 flex items-center justify-center gap-x-6">
