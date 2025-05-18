48a9392ca618163206abdf5955224f113b446bdc
nevsky118 <nevsky118@gmail.com>
2025-05-18 23:01:01 +0300

    feat: demo

--- frontend/src/app/courses/layout.tsx
+++ frontend/src/app/courses/layout.tsx
@@ -1,13 +1,18 @@
 import { SiteFooter } from "@widgets/site-footer"
 import { SiteHeader } from "@widgets/site-header"
+import { Metadata } from "next"
 
 interface CoursesLayoutProps {
   children: React.ReactNode
 }
 
+export const metadata: Metadata = {
+  
+}
+
 export default function CoursesLayout({ children }: CoursesLayoutProps) {
   return (
-    <div data-wrapper="" className="border-border flex flex-1 flex-col">
+      <div data-wrapper="" className="border-border flex flex-1 flex-col">
       <div className="border-border mx-auto flex w-full flex-1 flex-col min-[1800px]:max-w-[1536px] min-[1800px]:border-x">
         <SiteHeader sticky={false} />
         <main className="flex-1">{children}</main>
--- frontend/src/shared/components/mdx-components.tsx
+++ frontend/src/shared/components/mdx-components.tsx
@@ -20,6 +20,9 @@ import { Separator } from "@ui/separator"
 import { Slot } from "@ui/slot"
 import { Tabs, TabsContent, TabsList, TabsTrigger } from "@ui/tabs"
 import { cn } from "@lib/utils"
+import { Badge } from "@ui/badge"
+import { Table, TableCell, TableHead, TableRow } from "@ui/table"
+import { GNS3Node } from "@components/gns3-node"
 
 const components = {
   Accordion,
@@ -32,6 +35,8 @@ const components = {
   Separator,
   Callout,
   Quiz,
+  Badge,
+  GNS3Node,
   h1: ({ className, ...props }: React.HTMLAttributes<HTMLHeadingElement>) => (
     <h1
       className={cn(
@@ -136,7 +141,7 @@ const components = {
   ),
   table: ({ className, ...props }: React.HTMLAttributes<HTMLTableElement>) => (
     <div className="my-6 w-full overflow-y-auto">
-      <table
+      <Table
         className={cn(
           "relative w-full overflow-hidden border-none text-sm",
           className
@@ -146,13 +151,13 @@ const components = {
     </div>
   ),
   tr: ({ className, ...props }: React.HTMLAttributes<HTMLTableRowElement>) => (
-    <tr
+    <TableRow
       className={cn("last:border-b-none m-0 border-b", className)}
       {...props}
     />
   ),
   th: ({ className, ...props }: React.HTMLAttributes<HTMLTableCellElement>) => (
-    <th
+    <TableHead
       className={cn(
         "px-4 py-2 text-left font-bold [&[align=center]]:text-center [&[align=right]]:text-right",
         className
@@ -161,7 +166,7 @@ const components = {
     />
   ),
   td: ({ className, ...props }: React.HTMLAttributes<HTMLTableCellElement>) => (
-    <td
+    <TableCell
       className={cn(
         "px-4 py-2 text-left [&[align=center]]:text-center [&[align=right]]:text-right",
         className
